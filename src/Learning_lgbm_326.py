bcea6fbaea3cff4894c65510d0396de85db752e0
MitsuruFujiwara <fujiwara52jp@gmail.com>
2018-11-06 22:29:27 +0900

    LB#20

--- src/Learning_lgbm_326.py
+++ src/Learning_lgbm_326.py
@@ -76,17 +76,21 @@ def kfold_lightgbm(df, num_folds, stratified = False, debug= False):
     del df
     gc.collect()
 
+    ############################################################################
+    # Session Level predictions
+    ############################################################################
+
     # Cross validation model
-    folds = get_folds(df=train_df, n_splits=num_folds)
+    folds_session = get_folds(df=train_df, n_splits=num_folds)
 
     # Create arrays and dataframes to store results
-    oof_preds = np.zeros(train_df.shape[0])
-    sub_preds = np.zeros(test_df.shape[0])
+    oof_preds_session = np.zeros(train_df.shape[0])
+    sub_preds_session = np.zeros(test_df.shape[0])
     feature_importance_df = pd.DataFrame()
     feats = [f for f in train_df.columns if f not in EXCLUDED_FEATURES+['totals.transactionRevenue']]
 
     # k-fold
-    for n_fold, (train_idx, valid_idx) in enumerate(folds):
+    for n_fold, (train_idx, valid_idx) in enumerate(folds_session):
         train_x, train_y = train_df[feats].iloc[train_idx], np.log1p(train_df['totals.transactionRevenue'].iloc[train_idx])
         valid_x, valid_y = train_df[feats].iloc[valid_idx], np.log1p(train_df['totals.transactionRevenue'].iloc[valid_idx])
 
@@ -132,37 +136,137 @@ def kfold_lightgbm(df, num_folds, stratified = False, debug= False):
                         verbose_eval=100
                         )
 
-        oof_preds[valid_idx] = np.expm1(reg.predict(valid_x, num_iteration=reg.best_iteration))
-        sub_preds += np.expm1(reg.predict(test_df[feats], num_iteration=reg.best_iteration)) / num_folds
+        oof_preds_session[valid_idx] = np.expm1(reg.predict(valid_x, num_iteration=reg.best_iteration))
+        sub_preds_session += np.expm1(reg.predict(test_df[feats], num_iteration=reg.best_iteration)) / num_folds
 
         fold_importance_df = pd.DataFrame()
         fold_importance_df["feature"] = feats
         fold_importance_df["importance"] = np.log1p(reg.feature_importance(importance_type='gain', iteration=reg.best_iteration))
         fold_importance_df["fold"] = n_fold + 1
         feature_importance_df = pd.concat([feature_importance_df, fold_importance_df], axis=0)
-        print('Fold %2d RMSE : %.6f' % (n_fold + 1, np.sqrt(mean_squared_error(valid_y, np.log1p(oof_preds[valid_idx])))))
+        print('Fold %2d RMSE : %.6f' % (n_fold + 1, np.sqrt(mean_squared_error(valid_y, np.log1p(oof_preds_session[valid_idx])))))
         del reg, train_x, train_y, valid_x, valid_y
         gc.collect()
 
     # Full RMSEスコアの表示&LINE通知
-    full_rmse = np.sqrt(mean_squared_error(np.log1p(train_df['totals.transactionRevenue']), np.log1p(oof_preds)))
-    line_notify('Full RMSE score %.6f' % full_rmse)
+    full_rmse_session = np.sqrt(mean_squared_error(np.log1p(train_df['totals.transactionRevenue']), np.log1p(oof_preds_session)))
+#    line_notify('Session Level Full RMSE score %.6f' % full_rmse_session)
+
+    # session level feature importance
+    display_importances(feature_importance_df ,
+                        '../output/lgbm_importances_session.png',
+                        '../output/feature_importance_lgbm_session.csv')
+
+    ############################################################################
+    # User Level predictions
+    ############################################################################
+
+    train_df['predictions'] = oof_preds_session
+    test_df['predictions'] = sub_preds_session
+
+    # Aggregate data at User level
+    aggregations = {'totals.transactionRevenue': ['sum']}
+    for col in feats+['totals.transactionRevenue']:
+        aggregations[col] = ['sum', 'max', 'min', 'mean', 'median', 'std']
+
+    train_df_agg = train_df[feats+['fullVisitorId','totals.transactionRevenue']].groupby('fullVisitorId').agg(aggregations)
+    test_df_agg = test_df[feats + ['fullVisitorId','totals.transactionRevenue']].groupby('fullVisitorId').agg(aggregations)
+
+    train_df_agg.columns = pd.Index([e[0] + "_" + e[1].upper() for e in train_df_agg.columns.tolist()])
+    test_df_agg.columns = pd.Index([e[0] + "_" + e[1].upper() for e in test_df_agg.columns.tolist()])
+
+    # Cross validation model
+    folds_agg = get_folds(df=train_df_agg[['totals.pageviews_MEAN']].reset_index(), n_splits=num_folds)
+
+    # Create arrays and dataframes to store results
+    oof_preds_agg = np.zeros(train_df_agg.shape[0])
+    sub_preds_agg = np.zeros(test_df_agg.shape[0])
+    feature_importance_df_agg = pd.DataFrame()
+    feats_agg = [f for f in train_df_agg.columns if f not in EXCLUDED_FEATURES+['totals.transactionRevenue_SUM']]
+
+    # k-fold
+    for n_fold, (train_idx, valid_idx) in enumerate(folds_agg):
+        train_x, train_y = train_df_agg[feats_agg].iloc[train_idx], np.log1p(train_df_agg['totals.transactionRevenue_SUM'].iloc[train_idx])
+        valid_x, valid_y = train_df_agg[feats_agg].iloc[valid_idx], np.log1p(train_df_agg['totals.transactionRevenue_SUM'].iloc[valid_idx])
+
+        # set data structure
+        lgb_train = lgb.Dataset(train_x,
+                                label=train_y,
+                                free_raw_data=False)
+        lgb_test = lgb.Dataset(valid_x,
+                               label=valid_y,
+                               free_raw_data=False)
+
+        # パラメータは適当です
+        params ={
+                'device' : 'gpu',
+#                'gpu_use_dp':True,
+                'task': 'train',
+                'boosting': 'gbdt',
+                'objective': 'regression',
+                'metric': 'rmse',
+                'num_iteration': 10000,
+                'learning_rate': 0.02,
+                'num_leaves': 64,
+                'colsample_bytree': 0.553240348074409,
+                'subsample': 0.471522873020333,
+                'max_depth': 8,
+                'reg_alpha': 9.83318745912308,
+                'reg_lambda': 0.925142409255232,
+                'min_split_gain': 0.954402595384603,
+                'min_child_weight': 44,
+                'min_data_in_leaf': 79,
+                'verbose': -1,
+                'seed':int(2**n_fold),
+                'bagging_seed':int(2**n_fold),
+                'drop_seed':int(2**n_fold)
+                }
+
+        reg = lgb.train(
+                        params,
+                        lgb_train,
+                        valid_sets=[lgb_train, lgb_test],
+                        valid_names=['train', 'test'],
+                        early_stopping_rounds= 200,
+                        verbose_eval=100
+                        )
+
+        oof_preds_agg[valid_idx] = np.expm1(reg.predict(valid_x, num_iteration=reg.best_iteration))
+        sub_preds_agg += np.expm1(reg.predict(test_df_agg[feats_agg], num_iteration=reg.best_iteration)) / num_folds
+
+        fold_importance_df = pd.DataFrame()
+        fold_importance_df["feature"] = feats_agg
+        fold_importance_df["importance"] = np.log1p(reg.feature_importance(importance_type='gain', iteration=reg.best_iteration))
+        fold_importance_df["fold"] = n_fold + 1
+        feature_importance_df_agg = pd.concat([feature_importance_df_agg, fold_importance_df], axis=0)
+        print('Fold %2d RMSE : %.6f' % (n_fold + 1, np.sqrt(mean_squared_error(valid_y, np.log1p(oof_preds_agg[valid_idx])))))
+        del reg, train_x, train_y, valid_x, valid_y
+        gc.collect()
+
+    # Full RMSEスコアの表示&LINE通知
+    full_rmse_agg = np.sqrt(mean_squared_error(np.log1p(train_df_agg['totals.transactionRevenue_SUM']), np.log1p(oof_preds_agg)))
+    line_notify('Visitor Level Full RMSE score %.6f' % full_rmse_agg)
+
+    # session level feature importance
+    display_importances(feature_importance_df_agg ,
+                        '../output/lgbm_importances_agg.png',
+                        '../output/feature_importance_lgbm_agg.csv')
 
     if not debug:
         # 提出データの予測値を保存
-        test_df.loc[:,'PredictedLogRevenue'] = sub_preds
-        submission = test_df.groupby('fullVisitorId').agg({'PredictedLogRevenue' : 'sum'}).reset_index()
+        test_df_agg.loc[:,'PredictedLogRevenue'] = sub_preds_agg
+        submission = test_df_agg[['PredictedLogRevenue']]
         submission['PredictedLogRevenue'] = np.log1p(submission['PredictedLogRevenue'])
         submission['PredictedLogRevenue'] =  submission['PredictedLogRevenue'].apply(lambda x : 0.0 if x < 0 else x)
         submission['PredictedLogRevenue'] = submission['PredictedLogRevenue'].fillna(0)
-        submission.to_csv(submission_file_name, index=False)
+        submission.to_csv(submission_file_name, index=True)
 
         # out of foldの予測値を保存
-        train_df['OOF_PRED'] = oof_preds
-        train_df[['fullVisitorId', 'OOF_PRED']].to_csv(oof_file_name, index= False)
+        train_df_agg['OOF_PRED'] = oof_preds_agg
+        train_df_agg[['OOF_PRED']].to_csv(oof_file_name, index= True)
 
         # API経由でsubmit
-        submit(submission_file_name, comment='cv: %.6f' % full_rmse)
+        submit(submission_file_name, comment='cv: %.6f' % full_rmse_agg)
 
     return feature_importance_df
 
@@ -172,8 +276,7 @@ def main(debug = False):
         df = get_df(num_rows)
         print("df shape:", df.shape)
     with timer("Run LightGBM with kfold"):
-        feat_importance = kfold_lightgbm(df, num_folds=NUM_FOLDS, stratified=False, debug=debug)
-        display_importances(feat_importance ,'../output/lgbm_importances.png', '../output/feature_importance_lgbm.csv')
+        kfold_lightgbm(df, num_folds=NUM_FOLDS, stratified=False, debug=debug)
 
 if __name__ == "__main__":
     submission_file_name = "../output/submission.csv"
